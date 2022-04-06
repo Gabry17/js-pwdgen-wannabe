a8b71d854a004d3455373879147939924e044e54
Gabry17 <gbianchi528@gmail.com>
2022-04-06 14:35:15 +0200

    inserito informazioni da chiedere all'utente

---
+++ js/script.js
@@ -0,0 +1,22 @@
+//DICHIARAZIONE
+//informazioni per l'utente
+
+//nome
+const userName = prompt("Qual/'è il tuo nome?");
+console.log(userName);
+
+//cognome
+const lastName = prompt("Qual/'è il tuo cognome?");
+console.log(lastName);
+
+//colore preferito
+const colorName = prompt("Qual/è il tuo colore preferito?");
+console.log(colorName);
+
+
+//ELABORAZIONE
+//esecuzione informazioni
+
+
+//OUTPUT
+//visualizzare informazioni nel browser
