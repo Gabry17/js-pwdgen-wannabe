d39379ac2c722caf4e638988e174a02d9801378c
Gabry17 <gbianchi528@gmail.com>
2022-04-06 14:43:25 +0200

    aggiunto elaborazione dei dati

--- js/script.js
+++ js/script.js
@@ -2,21 +2,25 @@
 //informazioni per l'utente
 
 //nome
-const userName = prompt("Qual/'è il tuo nome?");
+const userName = prompt("Qual'è il tuo nome?");
 console.log(userName);
 
 //cognome
-const lastName = prompt("Qual/'è il tuo cognome?");
+const lastName = prompt("Qual'è il tuo cognome?");
 console.log(lastName);
 
 //colore preferito
-const colorName = prompt("Qual/è il tuo colore preferito?");
+const colorName = prompt("Qual'è il tuo colore preferito?");
 console.log(colorName);
+
+//Data
+let year = 2022;
 
 
 //ELABORAZIONE
 //esecuzione informazioni
-
+let infUser = `Nome: ${userName} Cognome: ${lastName} Colore preferito: ${colorName} Anno: ${year}`;
+console.log(infUser);
 
 //OUTPUT
 //visualizzare informazioni nel browser
